8b5f7d0d5807c202f65c079dae2b33ce13b0883f
chinmayjog13 <37167529+chinmayjog13@users.noreply.github.com>
2018-03-31 04:56:43 +0530

    Update and rename main_Error_Diffusion.cpp to Error_Diffusion.cpp

--- Error_Diffusion.cpp
+++ Error_Diffusion.cpp
@@ -1,28 +1,72 @@
-/*
-// Author Chinmay Jog
-// Id: 6171795819
-// Email: cjog@usc.edu
-// Date: Feb 25, 2018
-// Assignment 2
-// Question 2- Error Diffusion
-*/#include <stdio.h>#include <iostream>#include <stdlib.h>
+/* Dithering by Error diffusion techniques
+   Output_Image(1) is output by Floyd Steinberg diffusion
+   Output_Image(2) is output by JJN diffusion
+   Output_Image(3) is output by Stucki diffusion
+   Pass arguments in following order- Input_Image.raw Output_Image(1).raw BytesPerPixel Width Height Output_Image(2).raw Output_Image(3).raw
+   Author- Chinmay Jog
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
 #include <cstdlib>
 #include <cmath>
-#include <math.h>using namespace std;
+#include <math.h>
+
+using namespace std;
 
 double* diffusion_fs(double* resultint_fs, int xSize, int ySize);
 double* diffusion_jjn(double* resultint_jjn, int xSize, int ySize);
 double* diffusion_st(double* resultint_st, int xSize, int ySize);
-int main(int argc, char *argv[]){	// Define file pointer and variables	FILE *file;	int BytesPerPixel;	int xSize = 512;
-	int ySize = 512;	// Check for proper syntax	if (argc < 3){		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;		return 0;	}	// Check if image is grayscale or color	if (argc < 4){		BytesPerPixel = 1; // default is grey image	}	else {		BytesPerPixel = atoi(argv[3]);		// Check if size is specified		if (argc >= 5){			xSize = atoi(argv[4]);
-			ySize = atoi(argv[5]);		}	}	// Allocate image data array	unsigned char* Imagedata = new unsigned char [xSize*ySize];
+
+int main(int argc, char *argv[])
+
+{
+	// Define file pointer and variables
+	FILE *file;
+	int BytesPerPixel;
+	int xSize = 512;
+	int ySize = 512;
+
+	// Check for proper syntax
+	if (argc < 3){
+		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;
+		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;
+		return 0;
+	}
+
+	// Check if image is grayscale or color
+	if (argc < 4){
+		BytesPerPixel = 1; // default is grey image
+	}
+	else {
+		BytesPerPixel = atoi(argv[3]);
+		// Check if size is specified
+		if (argc >= 5){
+			xSize = atoi(argv[4]);
+			ySize = atoi(argv[5]);
+		}
+	}
+
+	// Allocate image data array
+	unsigned char* Imagedata = new unsigned char [xSize*ySize];
 	unsigned char* result_fs = new unsigned char [xSize*ySize];
 	unsigned char* result_jjn = new unsigned char [xSize*ySize];
 	unsigned char* result_st = new unsigned char [xSize*ySize];
 	double* resultint_fs = new double [xSize*ySize];
 	double* resultint_jjn = new double [xSize*ySize];
 	double* resultint_st = new double [xSize*ySize];
-	// Read image (filename specified by first argument) into image data matrix	if (!(file=fopen(argv[1],"rb"))) {		cout << "Cannot open file: " << argv[1] <<endl;		exit(1);	}	fread(Imagedata, sizeof(unsigned char), xSize*ySize, file);	fclose(file);	//Error Diffusion
+
+
+	// Read image (filename specified by first argument) into image data matrix
+	if (!(file=fopen(argv[1],"rb"))) {
+		cout << "Cannot open file: " << argv[1] <<endl;
+		exit(1);
+	}
+	fread(Imagedata, sizeof(unsigned char), xSize*ySize, file);
+	fclose(file);
+
+	//Error Diffusion
 	for (int i = 0; i < xSize*ySize; i++)
     {
         resultint_fs[i] = Imagedata[i];
@@ -40,11 +84,29 @@ double* diffusion_st(double* resultint_st, int xSize, int ySize);
         result_st[i] = temp_st[i];
     }
     cout << "Diffusion done " << endl;
-	// Write image data (filename specified by second argument) from image data matrix	if (!(file=fopen(argv[2],"wb"))) {		cout << "Cannot open file: " << argv[2] << endl;		exit(1);	}	fwrite(result_fs, sizeof(unsigned char), xSize*ySize, file);	fclose(file);
 
-	if (!(file=fopen(argv[6],"wb"))) {		cout << "Cannot open file: " << argv[6] << endl;		exit(1);	}	fwrite(result_jjn, sizeof(unsigned char), xSize*ySize, file);	fclose(file);
+	// Write image data (filename specified by second argument) from image data matrix
 
-	if (!(file=fopen(argv[7],"wb"))) {		cout << "Cannot open file: " << argv[7] << endl;		exit(1);	}	fwrite(result_st, sizeof(unsigned char), xSize*ySize, file);	fclose(file);
+	if (!(file=fopen(argv[2],"wb"))) {
+		cout << "Cannot open file: " << argv[2] << endl;
+		exit(1);
+	}
+	fwrite(result_fs, sizeof(unsigned char), xSize*ySize, file);
+	fclose(file);
+
+	if (!(file=fopen(argv[6],"wb"))) {
+		cout << "Cannot open file: " << argv[6] << endl;
+		exit(1);
+	}
+	fwrite(result_jjn, sizeof(unsigned char), xSize*ySize, file);
+	fclose(file);
+
+	if (!(file=fopen(argv[7],"wb"))) {
+		cout << "Cannot open file: " << argv[7] << endl;
+		exit(1);
+	}
+	fwrite(result_st, sizeof(unsigned char), xSize*ySize, file);
+	fclose(file);
 
 	delete[] Imagedata;
 	delete[] result_fs;
@@ -53,7 +115,9 @@ double* diffusion_st(double* resultint_st, int xSize, int ySize);
 	delete[] resultint_fs;
 	delete[] resultint_jjn;
 	delete[] resultint_st;
-	return 0;}
+
+	return 0;
+}
 
 
 double* diffusion_fs(double* resultint, int xSize, int ySize)
